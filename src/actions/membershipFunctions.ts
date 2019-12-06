331a3fc98f94b786e2cc821a71a229ea63e9705b
Christopher Jativa <cjativa@barnesfoundation.org>
2019-12-06 14:25:43 -0500

    returns membership

--- src/actions/membershipFunctions.ts
+++ src/actions/membershipFunctions.ts
@@ -6,10 +6,12 @@ import { GetMembershipPayload } from '../interfaces/acmeMembershipPayloads';
  *  Providing an invalid or non-existent membership id will return empty
  * @param membershipId - The id of the membership to retrieve
  */
-const getMembership = async (membershipId: string) => {
+const getMembership = async (membershipId: string): Promise<GetMembershipPayload> => {
 	
 	const membershipUrl = `${GET_MEMBERSHIP}/${membershipId}`;
 	const membership = await performRequest(membershipUrl, 'get') as GetMembershipPayload;
+
+	return membership;
 }
 
 export const MembershipFunctions = {
