e89981e7e9dee08b4a4f4a9600c1fabe82a1b0ac
Christopher Jativa <cjativa@barnesfoundation.org>
2019-12-09 15:52:28 -0500

    added function for getting single event and typed the payload

--- src/actions/eventFunctions.ts
+++ src/actions/eventFunctions.ts
@@ -1,15 +1,15 @@
 import { performRequest } from './acmeRequestor';
-import { LIST_EVENTS, LIST_EVENT_SUMMARIES } from '../utils/acmeEndpoints';
-import { ListEventSummariesPayload } from '../interfaces/acmeEventPayloads';
+import { LIST_EVENTS, LIST_EVENT_SUMMARIES, GET_EVENT } from '../utils/acmeEndpoints';
+import { ListEventSummariesPayload, Event, ListEventsPayload } from '../interfaces/acmeEventPayloads';
 
-/** Returns a list of event instances that match the applied filters
+/** Returns a list of events that match the applied filters
  * @param startTime - ISO8601 date of events you want after this time (optional)
  * @param endTime - ISO8601 date of events you want before this time (optional)
  * @param saleChannel - Limit events to those that support the specified sale channel
  * @param templateId - The id of the event template you want the events for.  Only used if there is a startTime (optional)
  * @param type - One of private, standard or all.  If left off or standard then will not return events that were made for private event templates (optional)
  */
-async function listEvents(startTime?: string, endTime?: string, saleChannel?: string, templateId?: string, type?: string): Promise<any> {
+async function listEvents(startTime?: string, endTime?: string, saleChannel?: string, templateId?: string, type?: string): Promise<ListEventsPayload> {
 
 	let params = {};
 
@@ -20,8 +20,8 @@ async function listEvents(startTime?: string, endTime?: string, saleChannel?: st
 	templateId && (params = { ...params, templateId });
 	type && (params = { ...params, type });
 
-	const events = await performRequest(LIST_EVENTS, 'get', null, null, params);
-	return events;
+	const payload = await performRequest(LIST_EVENTS, 'get', null, null, params) as ListEventsPayload;
+	return payload;
 }
 
 /** Returns a list of event summaries that match the applied filters
@@ -40,11 +40,23 @@ async function listEventSummaries(startTime?: string, endTime?: string, saleChan
 	saleChannel && (params = { ...params, saleChannel });
 	type && (params = { ...params, type });
 
-	const events = await performRequest(LIST_EVENT_SUMMARIES, 'get', null, null, params) as ListEventSummariesPayload;
-	return events;
+	const payload = await performRequest(LIST_EVENT_SUMMARIES, 'get', null, null, params) as ListEventSummariesPayload;
+	return payload;
+}
+
+/** Returns an event object for the specified event id 
+ * @param eventId - The id of the event to retrieve
+ */
+async function getEvent(eventId: string): Promise<Event> {
+
+	const url = `${GET_EVENT}/${eventId}`;
+
+	const payload = await performRequest(url, 'get') as Event;
+	return payload;
 }
 
 export const EventFunctions = {
 	listEvents,
-	listEventSummaries
+	listEventSummaries,
+	getEvent
 }
--- src/interfaces/acmeEventPayloads.ts
+++ src/interfaces/acmeEventPayloads.ts
@@ -3,6 +3,139 @@ export interface ListEventsPayload {
 }
 
 export interface Event {
+	id: string,
+	name: string,
+	description: string,
+	templateId: string,
+	emailConfirmationMessage: string,
+	capacity: number,
+	startTime: string,
+	endTime: string,
+	resources: Resource[],
+	priceList: { prices: Price[], priceListId: string },
+	resourceDetails: any[],
+	scheduleId: string,
+	salesRestrictions: string[],
+	ticketDetails: TicketDetails,
+	manuallyDeleted: boolean,
+	fieldStatus: FieldStatus,
+	originalFields: { startTime: string, endTime: string, flexPays: any },
+	emailCancellationMessage: string,
+	state: string,
+	ticketConfig: { printerConfigs: any[], assigned: boolean },
+	customFields: { name: string, value: string }[],
+	offSchedule: boolean,
+	colorCategory: { backgroundColor: string, textColor: string },
+	images: { id: string, primary: boolean, thumbnail: string, highRes: string, screen: string, preview: string }[],
+	soldQuantity: number,
+	salesChannels: { name: string, channel: string, enabled: boolean }[],
+	type: string,
+	checkInCount: number,
+	available: boolean,
+	admissionType: string,
+	lastModified: string,
+	eventResourceSummary: EventResourceSummary
+}
+
+interface EventResourceSummaryValue {
+	resourceCategory: string,
+	totalRequestsCount: number,
+	fulfilledRequestsCount: number,
+	resourceNames: string[]
+}
+
+interface EventResourceSummary {
+	people: EventResourceSummaryValue,
+	place: EventResourceSummaryValue,
+	equipment: EventResourceSummaryValue,
+	totalRequestsCount: number,
+	fulfilledRequestsCount: number,
+	resourceFulfillmentStatus: string,
+	hasResourceRequests: boolean
+}
+
+interface FieldStatusValue {
+	modified: boolean,
+	locked: boolean
+}
+
+interface FieldStatus {
+	name: FieldStatusValue,
+	description: FieldStatusValue,
+	emailConfirmationMessage: FieldStatusValue,
+	capacity: FieldStatusValue,
+	startTime: FieldStatusValue,
+	endTime: FieldStatusValue,
+	priceList: FieldStatusValue,
+	resources: FieldStatusValue,
+	ticketDetails: FieldStatusValue,
+	images: FieldStatusValue,
+	shortDescription: FieldStatusValue,
+	flexPays: FieldStatusValue,
+	emailCancellationMessage: FieldStatusValue,
+	ticketConfig: FieldStatusValue,
+	orderForms: FieldStatusValue,
+	customFields: FieldStatusValue,
+	ticketDurationDays: FieldStatusValue,
+	modified: boolean
+}
+
+interface TicketDetails {
+	ticketDurationDays: number,
+	checkInWindow: {
+		minutesBefore: number,
+		minutesAfter: number,
+		blockScan: boolean,
+		autoRebook: boolean
+	},
+	postStartPurchase: {
+		allowLateTicketPurchase: boolean,
+		minutesAfter: number
+	},
+	purchaseAvailability: {
+		timeFrame: string,
+		minutes: number
+	},
+	ticketPrintLines: string[],
+	doNotPrint: boolean,
+	expireFromEventDate: boolean,
+	hideEventDate: boolean
+}
+
+interface Price {
+	personType: {
+		id: string,
+		name: string,
+		description: string,
+		active: boolean,
+		type: string,
+		displayOrder: number
+	},
+	price: string,
+	salesChannels: {
+		name: string,
+		channel: string,
+		enabled: boolean
+	}[],
+	displayOrder: number,
+	discountedPrice: string,
+	discount: string
+}
+
+interface Resource {
+	resourceTemplateId: string,
+	resourceTemplateName: string,
+	type: string,
+	quantity: number,
+	resourceCategory: string,
+	values: any[],
+	resourceStartTime: string,
+	resourceEndTime: string,
+	eventScheduleResourceRequestId: string,
+	notes: string,
+	eventResourceId: string,
+	eventResourceRequestType: string,
+	eventResourceRequestId: string
 }
 
 export interface ListEventSummariesPayload {
@@ -15,7 +148,7 @@ export interface EventSummary {
 	startTime: string,
 	endTime: string,
 	name: string,
-	sold: number, 
+	sold: number,
 	available: number,
 	checkedIn: number,
 	reserved: number,
