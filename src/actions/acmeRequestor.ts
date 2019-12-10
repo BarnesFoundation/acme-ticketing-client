e339860e1616f26c37716a3b735fe85f3b99b1f7
Christopher Jativa <cjativa@barnesfoundation.org>
2019-12-10 14:33:21 -0500

    refactored providing of request config

--- src/actions/acmeRequestor.ts
+++ src/actions/acmeRequestor.ts
@@ -2,17 +2,30 @@ import axios, { Method } from 'axios';
 import { clientConfig } from '../index';
 
 
-export const performRequest = async (url: string, method: Method, additionalHeaders?: {}, data?: any, params?: { }): Promise<any> => {
+interface RequestConfig {
+	url: string,
+	method: string,
+	additionalHeaders?: {},
+	data?: any,
+	params?: {},
+}
 
-	const baseURL = clientConfig.apiRootUrl;
-	const defaultHeaders = { 'x-acme-api-key': clientConfig.apiKey };
+const baseURL = clientConfig.apiRootUrl;
+const defaultHeaders: {} = { 'x-acme-api-key': clientConfig.apiKey };
+
+export const performRequest = async (config: RequestConfig): Promise<any> => {
 
 	let headers = { ...defaultHeaders };
 
+	// Grab the params from the config and cast Method
+	const { url, method: providedMethod, additionalHeaders, data, params } = config;
+	const method = providedMethod as Method;
+
 	// If additional headers were provided, merge them
-	if (additionalHeaders) {
-		headers = { ...headers, ...additionalHeaders };
-	}
+	if (additionalHeaders) { headers = { ...headers, ...additionalHeaders }; }
+
+	// If the request is to a b2c endpoint
+	if (url.includes('/b2c/')) { headers = { ...headers, 'x-b2c-tenant-id': clientConfig.b2cTenantId }; }
 
 	try {
 		const response = (await axios({ baseURL, headers, url, method, data, params })).data;
