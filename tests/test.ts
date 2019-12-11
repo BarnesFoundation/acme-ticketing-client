93a6ad3dca93f8490d9adc35cf6fb37b0302c572
Christopher Jativa <cjativa@barnesfoundation.org>
2019-12-11 16:16:03 -0500

    tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,4 +1,5 @@
-import { ACMETicketingClient, TicketingFunctions, EventFunctions } from '../src/index';
+import { ACMETicketingClient, TicketingFunctions, EventFunctions, MembershipSummaryFunctions, OrderFunctions, MembershipCardFunctions } from '../src/index';
+import { TicketingFunctions as tfc } from '../src/convenience';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -8,10 +9,14 @@ const main = async () => {
 	// Get credentials from .env
 	const b2cTenantId = process.env.B2C_TENANT_ID;
 	const apiKey = process.env.API_KEY;
-	const apiRootUrl = process.env.API_ROOT_URL;
+	// const apiRootUrl = process.env.API_ROOT_URL;
 
 	// Setup client
-	const ac = new ACMETicketingClient({ b2cTenantId, apiKey, apiRootUrl });
+	const ac = new ACMETicketingClient({ b2cTenantId, apiKey });
+
+	const r = await tfc.getTicketsForMembershipCard('3320311');
+	console.log(r);
+
 }
 
 main(); 
