bb7950b4ac94301f31bf9bab7df2d35596fe7cda
Christopher Jativa <cjativa@barnesfoundation.org>
2020-01-15 16:18:26 -0500

    beginning adding template times fetch

--- src/actions/core/checkoutManagement/eventTemplateB2CFunctions.ts
+++ src/actions/core/checkoutManagement/eventTemplateB2CFunctions.ts
@@ -28,9 +28,47 @@ export interface EventTemplateSummariesParameters extends EventTemplateParameter
 	saleChannel: 'online' | 'customerRep' | 'pointOfSale' | 'manualEntry'
 }
 
+/** Returns the event template summaries matching the specified input params
+ * @params params - Input params event template summaries should match (optional)
+ */
 export async function listEventTemplateSummaries(params?: EventTemplateSummariesParameters): Promise<EventTemplateSummaryB2C[]> {
 
 	const payload = await performRequest({ url: LIST_EVENT_TEMPLATES_SUMMARIES_B2C, method: 'get', params }) as EventTemplateSummaryB2C[];
 
+	return payload;
+}
+
+/** Object for the Event Templates input parameters to provide. Optional */
+export interface TemplateTimesParameters {
+
+	/** The template id you want to list the times for. Required */
+	id: string,
+
+	/** ISO8601 date string. The time you want the events to be after (optional) */
+	startTime?: string,
+
+	/** ISO8601 date string. The time you want your events to be before (optional) */
+	endTime?: string,
+
+	/** One of private, standard or all.  If left off or standard then will not return events that were made for private event templates (optional) */
+	type?: 'private' | 'standard' | 'all',
+
+	/** The current user's membership id. This allows for times for schedules that are only accessible to memberships to be returned. (optional) */
+	membershipId?: string,
+
+	/** The current user's membership category. This allows for times for schedules that are only accessible to memberships to be returned */
+	membershipCategoryId?: string
+
+	 /** The combo template Id contains a list of templates (optional) */
+	 comboTemplateId?: string
+}
+
+
+/** Returns a list of times for events of a specified event template
+ * @params paramrs - Input params event template summaries should match (optional)
+ */
+export async function listTemplateTimes(params?: TemplateTimesParameters): Promise<any[]> {
+
+	const payload = await performRequest({ url: LIST_TEMPLATE_TIMES(params.id), method: 'get', params });
 	return payload;
 }
