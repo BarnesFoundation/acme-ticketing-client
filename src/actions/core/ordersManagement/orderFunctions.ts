7e15fb533fb4a277c43843d4c5cd681d9c315e8b
Christopher Jativa <cjativa@barnesfoundation.org>
2020-07-01 12:34:10 -0400

    added private order search functionality

--- src/actions/core/ordersManagement/orderFunctions.ts
+++ src/actions/core/ordersManagement/orderFunctions.ts
@@ -1,6 +1,6 @@
 import { performRequest } from '../../acmeRequestor';
-import { GET_ORDER, GET_ORDERS_FOR_EVENT } from '../../../utils/acmeEndpoints';
-import { Order } from '../../../interfaces/acmeOrderPayloads';
+import { GET_ORDER, SEARCH_ORDERS, GET_ORDERS_FOR_EVENT } from '../../../utils/acmeEndpoints';
+import { Order, SearchOrdersPayload } from '../../../interfaces/acmeOrderPayloads';
 
 
 /** Returns an order object for the specified order id 
@@ -46,6 +46,33 @@ async function listOrdersForEvent(eventId: string): Promise<Order[]> {
 	return payload;
 }
 
+interface SearchOrdersParams extends OrderParameters {
+
+	/** Field by which results are sorted */
+	sortField?: 'createdOn',
+
+	/** Results sorting direction (“asc” or “desc”) */
+	sortDirection?: 'asc' | 'desc',
+
+	/** Page number of the displayed results */
+	page?: number,
+
+	/** Number of records included in each page. Recommended to 1000 or less */
+	pageSize?: number,
+
+	/** Total number of records returned by this call */	
+	count?: number
+}
+
+/** This is not a publicly available API endpoint for Orders. It allows for searching, sorting, and pagination on an orders request.
+ * Functions similarly to OrderFunctions.listOrders, but accepts additional sort and pagination params and returns them in a paginated response
+ */
+async function searchOrders(params: SearchOrdersParams): Promise<SearchOrdersPayload> {
+
+	const payload = await performRequest({ url: SEARCH_ORDERS, method: 'get', params }) as SearchOrdersPayload;
+	return payload;
+}
+
 /** Module for the Orders Management - Orders endpoints. 
  * When utilizing this module, your ACME client config object **must** have the `b2cTenantId` value set. Otherwise, calls to methods belonging to this module will fail. */
-export { getOrder, listOrders, listOrdersForEvent }
+export { getOrder, listOrders, listOrdersForEvent, searchOrders };
