b3ec1c71850077a811a92bc692427ae7bb224104
Christopher Jativa <cjativa@barnesfoundation.org>
2021-09-26 21:12:38 -0400

    refactoring some function-style endpoints

--- src/utils/acmeEndpoints.ts
+++ src/utils/acmeEndpoints.ts
@@ -10,11 +10,15 @@ export const GET_EVENT = "/v2/b2b/event/instances";
 
 // Event Management - Event Template endpoints
 export const LIST_EVENT_TEMPLATES = "/v2/b2b/event/templates";
-export const LIST_EVENT_TEMPLATES_SUMMARIES = "/v2/b2b/event/templates/summaries";
-export const GET_EVENT_TEMPLATE = (id: string | number) => { return `/v2/b2b/event/template/${id}`; };
+export const LIST_EVENT_TEMPLATES_SUMMARIES =
+    "/v2/b2b/event/templates/summaries";
+export const GET_EVENT_TEMPLATE = (id: string | number) => {
+    return `/v2/b2b/event/template/${id}`;
+};
 
 // Event Management - Event Images endpoints
-export const READ_EVENT_TEMPLATE_IMAGE = (templateId: string, size: string) => (`/v2/b2b/photos/program/${templateId}/${size}`);
+export const READ_EVENT_TEMPLATE_IMAGE = (templateId: string, size: string) =>
+    `/v2/b2b/photos/program/${templateId}/${size}`;
 
 // Membership endpoints
 export const GET_MEMBERSHIP = "/v2/b2b/memberships";
@@ -27,12 +31,14 @@ export const GET_MEMBERSHIP_SUMMARIES = "/v2/b2b/membership/summaries";
 
 // Membership Levels endpoints
 export const LIST_MEMBERSHIP_LEVELS = "/v2/b2b/membership/levels";
-export const GET_MEMBERSHIP_LEVEL = (id: string) => `/v2/b2b/membership/levels/${id}`;
-export const VALIDATE_MEMBERSHIP_CHECKOUT = "/v2/b2b/memberships/checkout/validate";
+export const GET_MEMBERSHIP_LEVEL = (id: string) =>
+    `/v2/b2b/membership/levels/${id}`;
+export const VALIDATE_MEMBERSHIP_CHECKOUT =
+    "/v2/b2b/memberships/checkout/validate";
 
 // Orders Management - Orders endpoints
 export const GET_ORDER = "/v2/b2c/orders";
-export const SEARCH_ORDERS ="/v2/b2c/orders/search";
+export const SEARCH_ORDERS = "/v2/b2c/orders/search";
 export const GET_ORDERS_FOR_EVENT = "/v2/b2c/orders/event";
 export const REFUND_ORDER = "/v1/b2b/b2b/refunds";
 export const UPDATE_ORDER = "/v2/b2b/orders/update";
@@ -50,14 +56,20 @@ export const EXECUTE_ADHOC_REPORT = "/v2/b2b/analytics/report/execute";
 
 // Checkout Management - Event Template B2C endpoints
 export const LIST_EVENT_TEMPLATES_B2C = "/v2/b2c/event/templates";
-export const LIST_EVENT_TEMPLATE_INSTANCES_B2C = (eventTemplateId: string) => `v2/b2c/event/templates/${eventTemplateId}/instances`;
-export const LIST_EVENT_TEMPLATES_SUMMARIES_B2C = "/v2/b2c/event/templates/summaries";
+export const LIST_EVENT_TEMPLATE_INSTANCES_B2C = (eventTemplateId: string) =>
+    `v2/b2c/event/templates/${eventTemplateId}/instances`;
+export const LIST_EVENT_TEMPLATES_SUMMARIES_B2C =
+    "/v2/b2c/event/templates/summaries";
 export const LIST_TEMPLATE_TIMES = (id?: string | number) => {
-	if (id) return `/v2/b2c/event/templates/${id}/times`;
-	return `/v2/b2c/event/templates/times`;
+    if (id) return `/v2/b2c/event/templates/${id}/times`;
+    return `/v2/b2c/event/templates/times`;
 };
-export const GET_EVENT_TEMPLATE_B2C = (id: string | number) => { return `/v2/b2c/event/templates/${id}`; };
-export const GET_ACTIVITY_CALENDAR_FOR_TEMPLATE = (id: string | number) => { return `/v2/b2c/event/templates/${id}/calendar`; }
+export const GET_EVENT_TEMPLATE_B2C = (id: string | number) => {
+    return `/v2/b2c/event/templates/${id}`;
+};
+export const GET_ACTIVITY_CALENDAR_FOR_TEMPLATE = (id: string | number) => {
+    return `/v2/b2c/event/templates/${id}/calendar`;
+};
 
 // Checkout Management - E-Commerce B2C Check endpoints
 export const B2C_CHECKOUT = "/v2/b2c/checkout/";
@@ -69,7 +81,10 @@ export const ENTITLEMENTS_VALIDATE = "/v2/b2c/entitlements/validate";
 // Checkout Management - B2B Checkout
 export const B2B_CHECKOUT = "/v2/b2b/checkout";
 export const B2B_CHECKOUT_KIOSK = "/v2/b2b/checkout/kiosk/";
-export const B2B_SEND_ORDER_EMAIL = (orderNumber: string) => { return `/v2/b2b/orders/${orderNumber}/email`; };
+export const B2B_SEND_ORDER_EMAIL = (orderNumber: string) => {
+    return `/v2/b2b/orders/${orderNumber}/email`;
+};
 
 // Platform APIs - Add-Ons
 export const GET_ADD_ONS = "/v2/b2b/addons";
+export const GET_FORMS_RESPONSES = "/v2/b2c/forms/responses";
