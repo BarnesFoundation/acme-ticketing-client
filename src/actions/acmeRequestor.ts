b235967813740fb7572f554081b2244d3b60ccab
Christopher Jativa <cjativa@barnesfoundation.org>
2021-09-26 22:23:44 -0400

    performRquest is now generic" `

--- src/actions/acmeRequestor.ts
+++ src/actions/acmeRequestor.ts
@@ -1,76 +1,66 @@
-import axios, { Method } from 'axios';
-import { clientConfig } from '../../index';
-
+import axios, { Method } from "axios";
+import { clientConfig } from "../../index";
 
 interface RequestConfig {
-	url: string,
-	method: string,
-	additionalHeaders?: {},
-	data?: any,
-	params?: {},
-	throwRaw?: boolean,
+    url: string;
+    method: string;
+    additionalHeaders?: {};
+    data?: any;
+    params?: {};
+    throwRaw?: boolean;
 }
 
-export const performRequest = async (config: RequestConfig): Promise<any> => {
+export const performRequest = async <T>(config: RequestConfig): Promise<T> => {
+    // Setup request
+    const baseURL = clientConfig.apiRootUrl;
+    let headers: {} = {
+        "x-acme-api-key": clientConfig.apiKey,
+    };
 
-	// Setup request
-	const baseURL = clientConfig.apiRootUrl;
-	let headers: {} = {
-		'x-acme-api-key': clientConfig.apiKey,
-	};
+    // Grab the params from the config and cast Method
+    const { additionalHeaders, throwRaw = false } = config;
 
-	// Grab the params from the config and cast Method
-	const {
-		additionalHeaders,
-		throwRaw = false,
-	} = config;
+    // If additional headers were provided, merge them
+    if (additionalHeaders) {
+        headers = {
+            ...headers,
+            ...additionalHeaders,
+        };
+    }
 
-	// If additional headers were provided, merge them
-	if (additionalHeaders) {
-		headers = {
-			...headers,
-			...additionalHeaders
-		};
-	}
+    // If the request is to a b2c endpoint
+    // we'll add the tenant id
+    if (config.url.includes("/b2c/")) {
+        headers = {
+            ...headers,
+            "x-b2c-tenant-id": clientConfig.b2cTenantId,
+        };
+    }
 
-	// If the request is to a b2c endpoint
-	// we'll add the tenant id
-	if (config.url.includes('/b2c/')) {
-		headers = {
-			...headers,
-			'x-b2c-tenant-id': clientConfig.b2cTenantId
-		};
-	}
+    try {
+        const response = await axios({
+            baseURL,
+            headers,
+            method: config.method as Method,
+            url: config.url,
+            data: config.data,
+            params: config.params,
+        });
+        return response.data;
+    } catch (error) {
+        // If we are throwing the raw error, just throw the error object.
+        if (throwRaw) {
+            throw error;
+        }
+        // Otherwise, throw string.
+        else {
+            // If there's a data message for the error, it's more useful than the whole error stack
+            const errorMessage = error.response.data
+                ? JSON.stringify(error.response.data)
+                : error;
 
-	try {
-		const response = await axios({
-			baseURL,
-			headers,
-			method: config.method as Method,
-			url: config.url,
-			data: config.data,
-			params: config.params,
-		});
-		return response.data;
-	}
-
-	catch (error) {
-
-		// If we are throwing the raw error, just throw the error object.
-		if (throwRaw) {
-			throw error;
-		}
-		// Otherwise, throw string.
-		else {
-
-			// If there's a data message for the error, it's more useful than the whole error stack
-			const errorMessage = (error.response.data)
-			? JSON.stringify(error.response.data)
-			: error;
-
-			throw `An error occurred sending a ${config.method} request to endpoint ${config.url}. 
+            throw `An error occurred sending a ${config.method} request to endpoint ${config.url}. 
 				   Error was: ${errorMessage}`;
-		}
-
-	}
-}
+        }
+    }
+};
--- src/actions/core/platform/formFunctions.ts
+++ src/actions/core/platform/formFunctions.ts
@@ -1,21 +1,58 @@
 import { performRequest } from "../../acmeRequestor";
 import { GET_FORMS_RESPONSES } from "../../../utils/acmeEndpoints";
+import {
+    IPaginationRequestSettings,
+    IPaginatedResponse,
+} from "../../../interfaces/pagination";
+
+export interface IFormResponse {
+    customFields: any[];
+    formId: string;
+    id: string;
+    name: string;
+    number: string;
+    publishedFormTemplate: {
+        id: string;
+        lastPublishedDate: string;
+        lastUpdatedDate: string;
+        name: string;
+        oncePerCart: boolean;
+        source: string;
+        sourceFormDefinition: string;
+        sourceFormEditUrl: string;
+        sourceFormId: string;
+        sourceFormUrl: string;
+        state: string;
+        type: string;
+    };
+    responseData: {};
+    source: string;
+    submissionDate: string;
+    type: string;
+}
 
 /** Fetches a specified response for a form
  * @params id - the id of the form response
  *
  * Returns a JSON representation of FormResponseVo
  */
-export async function fetchFormResponse(id: string) {
-    const payload = await performRequest({
+export async function fetchFormResponse(id: string): Promise<IFormResponse> {
+    const payload = await performRequest<IFormResponse>({
         url: `${GET_FORMS_RESPONSES}/${id}`,
         method: "GET",
     });
+
+    return payload;
 }
 
-export async function fetchFormResponses() {
-    const payload = await performRequest({
+export async function fetchFormResponses(
+    params?: IPaginationRequestSettings
+): Promise<IPaginatedResponse<IFormResponse>> {
+    const payload = await performRequest<IPaginatedResponse<IFormResponse>>({
         url: `${GET_FORMS_RESPONSES}`,
         method: "GET",
+        params,
     });
-};
+
+    return payload;
+}
